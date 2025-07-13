43a2de6f11e0512e317b9e42f2dfc3c7ffcefce6
Millard1018 <millardlirio@gmail.com>
2025-07-13 15:02:58 +0800

    commit

---
+++ src/frontend/admin/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import postcss from '../../../postcss.config.js';
+export default defineConfig({
+  root: './src/frontend/admin',
+  build: {
+    outDir: '../../../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: './src/frontend/admin/admin.html'
+    }
+  },
+  css: {
+    postcss
+  }
+});
